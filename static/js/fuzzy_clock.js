fea95d57722ae4b6b009d853ceb68ebb1b262149
Noura <culurienneldoreth@gmail.com>
2014-10-05 14:37:16 -0700

    drawing hour markers for clock

--- static/js/fuzzy_clock.js
+++ static/js/fuzzy_clock.js
@@ -2,14 +2,40 @@ var fz = function() {
 
     var canvas = document.getElementById('clock');
     var ctx = canvas.getContext('2d');
+    var w = h = cx = cy = r = clock_r = hrs_r = min_r = 0;
 
     var init = function() {
-        var w = $(window).width() * .9;
-        var h = $(window).height() * .9;
+        w = $(window).width() * 0.9;
+        h = $(window).height() * 0.9;
         canvas.setAttribute('width', w);
         canvas.setAttribute('height', h);
         ctx.fillStyle = '#000000';
         ctx.fillRect(0, 0, w, h);
+
+        cx = Math.round(w / 2);
+        cy = Math.round(h / 2);
+        r = Math.min(w, h) / 2;
+        clock_r = r * 0.9;
+        hrs_r = r * 0.5;
+        min_r = r * 0.75;
+
+        for (var a = 0; a < Math.PI * 2; a += Math.PI / 6) {
+            var x = cx + Math.cos(a) * clock_r;
+            var y = cy + Math.sin(a) * clock_r;
+            draw_circle(x, y, 4, '#FFFFFF');
+        }
+    };
+
+    var draw = function() {
+
+    };
+
+    var draw_circle = function(_x, _y, _r, _color) {
+        ctx.beginPath();
+        ctx.arc(_x, _y, _r, 0, 2 * Math.PI);
+        ctx.fillStyle = _color;
+        ctx.fill();
+        ctx.stroke();
     };
 
     return {
