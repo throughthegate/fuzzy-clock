d2e1bed5fbc9fc9b655d57d8f6045d7ff553c379
Noura <culurienneldoreth@gmail.com>
2014-10-05 15:34:14 -0700

    circles draw themselves once to represent the time.

--- static/js/fuzzy_clock.js
+++ static/js/fuzzy_clock.js
@@ -1,29 +1,37 @@
-var hand_circle = function(cx, cy, polar_r, fuzziness, r, color) {
-    var draw = function() {
+var canvas, ctx;
 
+var hand_circle = function(cx, cy, polar_r, fuzziness, r, color, angle) {
+    var a = angle + fuzziness;
+    var x = cx + Math.cos(a) * polar_r;
+    var y = cy + Math.sin(a) * polar_r;
+    var draw = function() {
+        draw_circle(x, y, r, color); 
     };
     return {
         draw: draw,
     };
 };
 
-var clock_hand = function(cx, cy, r, fuzziness, n_circles, color) {
+var clock_hand = function(cx, cy, r, fuzziness, n_circles, color, angle) {
 
     var movement = 0.4;
-    var smallC = 7; // smallest circle radius
-    var largeC = 13; // biggest circle radius
+    var smallC = 3; // smallest circle radius
+    var largeC = 7; // biggest circle radius
     var circles = [];
 
     for (var i = 0; i < n_circles; i++) {
         circles.push(hand_circle(cx, cy,
-            (Math.random() * 0.9 + 0.1) * r,
-            (Math.random() * 2 - 1) * fuzziness,
-            (Math.random() * (largeC - smallC)) + smallC,
-            color));
+            map(Math.random(), 0, 1, 0.1, 0.9) * r,
+            map(Math.random(), 0, 1, -1, 1) * fuzziness,
+            map(Math.random(), 0, 1, smallC, largeC),
+            color,
+            angle));
     }
 
     var draw = function() {
-
+        for (var i = 0; i < circles.length; i++) {
+            circles[i].draw();
+        }
     };
 
     return {
@@ -33,10 +41,15 @@ var clock_hand = function(cx, cy, r, fuzziness, n_circles, color) {
 
 var fuzzy_clock = function() {
 
-    var canvas = document.getElementById('clock');
-    var ctx = canvas.getContext('2d');
+    canvas = document.getElementById('clock');
+    ctx = canvas.getContext('2d');
     var w = h = cx = cy = r = clock_r = hrs_r = min_r = 0;
     var hrs_hand = null, min_hand = null;
+
+    var draw = function() {
+        hrs_hand.draw();
+        min_hand.draw();
+    };
 
     var init = function() {
         w = $(window).width() * 0.9;
@@ -59,25 +72,45 @@ var fuzzy_clock = function() {
             draw_circle(x, y, 4, '#FFFFFF');
         }
 
-        hrs_hand = clock_hand(cx, cy, hrs_r, Math.PI * .1, 30, '#FF00FF');
-        min_hand = clock_hand(cx, cy, min_r, Math.PI * .3, 70, '#FFFF00');
-    };
+        hrs_hand = clock_hand(cx, cy, hrs_r, Math.PI * .1, 30, '#FF00FF', hour_angle());
+        min_hand = clock_hand(cx, cy, min_r, Math.PI * .3, 70, '#FFFF00', min_angle());
 
-    var draw = function() {
-
-    };
-
-    var draw_circle = function(_x, _y, _r, _color) {
-        ctx.beginPath();
-        ctx.arc(_x, _y, _r, 0, 2 * Math.PI);
-        ctx.fillStyle = _color;
-        ctx.fill();
-        ctx.stroke();
+        draw(); // TODO update times and redraw
     };
 
     return {
         init: init,
     };
+};
+
+var draw_circle = function(_x, _y, _r, _color) {
+    ctx.beginPath();
+    ctx.arc(_x, _y, _r, 0, 2 * Math.PI);
+    ctx.fillStyle = _color;
+    ctx.fill();
+    //ctx.stroke();
+};
+
+var map = function(val, domain_min, domain_max, range_min, range_max) {
+    return (val - domain_min) / (domain_max - domain_min) * (range_max - range_min) + range_min;
+};
+
+var hour_angle = function() { 
+    var d = new Date();
+    var hour = d.getHours();
+    var min = d.getMinutes();
+    var res = map(hour + min/60, 0, 24, 0, 2 * 2 * Math.PI)  - Math.PI / 2; 
+    console.log('hour_angle', res);
+    return res;
+};
+
+var min_angle = function(){
+    var d = new Date();
+    var min = d.getMinutes();
+    var sec = d.getSeconds();
+    var res = map(min + sec/60, 0, 60, 0, 2 * Math.PI) - Math.PI / 2;
+    console.log('min_angle', res);
+    return res;
 };
 
 window.onload = function() {
